c2a8459158befaf10a57e393b29fad4c10f5db52
fkafri <firas.alkafri@gmail.com>
2019-05-13 11:23:41 +0300

    more handlers

--- lms_events_handlers/handlers.py
+++ lms_events_handlers/handlers.py
@@ -1,5 +1,4 @@
 from django_microservice_propaganda.propaganda import Propaganda, logger
-from pytz import unicode
 
 from lms_events_handlers.lms_templates_data import get_new_enrollment_data, NEW_ENROLLMENT_SENDGRID_TEMPLATE_ID
 from salalem_notifications.models import EmailNotificationData
@@ -40,7 +39,6 @@ def on_enrollment_valid_until_extended_handler(body, message):
 
 
 def on_enrollment_allowed_attempts_changed_handler(body, message):
-    print("-------")
     notification_data = EmailNotificationData.from_json(body['enrollment'])
     template_data = get_new_enrollment_data(notification_data)
     send_email(AvailableEmailServiceProviders.sendgrid, to_emails=[notification_data.to],
@@ -54,11 +52,66 @@ def on_enrollment_allowed_attempts_changed_handler(body, message):
                ])
 
 
+def on_enrollment_updated_status_failed_handler(body, message):
+    notification_data = EmailNotificationData.from_json(body['enrollment'])
+    template_data = get_new_enrollment_data(notification_data)
+    send_email(AvailableEmailServiceProviders.sendgrid, to_emails=[notification_data.to],
+               template_id=NEW_ENROLLMENT_SENDGRID_TEMPLATE_ID,
+               template_data=template_data,
+               categories=[
+                   "lms",
+                   "enrollment",
+                   "graded",
+                   "status",
+                   "updated",
+                   "failed"
+               ])
+
+
+def on_enrollment_updated_status_resubmit_handler(body, message):
+    notification_data = EmailNotificationData.from_json(body['enrollment'])
+    template_data = get_new_enrollment_data(notification_data)
+    send_email(AvailableEmailServiceProviders.sendgrid, to_emails=[notification_data.to],
+               template_id=NEW_ENROLLMENT_SENDGRID_TEMPLATE_ID,
+               template_data=template_data,
+               categories=[
+                   "lms",
+                   "enrollment",
+                   "graded",
+                   "status",
+                   "updated",
+                   "failed"
+               ])
+
+
+def on_certificate_ready_handler(body, message):
+    notification_data = EmailNotificationData.from_json(body['certificate'])
+    template_data = get_new_enrollment_data(notification_data)
+    send_email(AvailableEmailServiceProviders.sendgrid, to_emails=[notification_data.to],
+               template_id=NEW_ENROLLMENT_SENDGRID_TEMPLATE_ID,
+               template_data=template_data,
+               categories=[
+                   "lms",
+                   "enrollment",
+                   "graded",
+                   "status",
+                   "updated",
+                   "failed"
+               ])
+
+
 logger.error('Subscribing now')
 print("--------")
 
-propaganda.subscribe("lms.enrollment.#")\
-    .on('lms.enrollment.new', on_new_enrollment_handler, on_exception=log_mq_exception)\
-    .on('lms.enrollment.updated.valid_until.extended', on_enrollment_valid_until_extended_handler, on_exception=log_mq_exception)\
-    .on('lms.enrollment.updated.allowed_attempts.changed', on_enrollment_allowed_attempts_changed_handler, on_exception=log_mq_exception)
-
+propaganda.subscribe("lms.enrollment.#") \
+    .on('lms.enrollment.new', on_new_enrollment_handler, on_exception=log_mq_exception) \
+    .on('lms.enrollment.updated.valid_until.extended', on_enrollment_valid_until_extended_handler,
+        on_exception=log_mq_exception) \
+    .on('lms.enrollment.updated.allowed_attempts.changed', on_enrollment_allowed_attempts_changed_handler,
+        on_exception=log_mq_exception) \
+    .on('lms.enrollment.updated.status.failed', on_enrollment_updated_status_failed_handler,
+        on_exception=log_mq_exception) \
+    .on('lms.enrollment.updated.status.resubmit', on_enrollment_updated_status_resubmit_handler,
+        on_exception=log_mq_exception) \
+    .on('lms.enrollment.certificate.status.ready', on_certificate_ready_handler,
+        on_exception=log_mq_exception)

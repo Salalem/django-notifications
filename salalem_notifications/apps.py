87fdab54ef747db6184b9e54508419962424c14e
fkafri <firas.alkafri@gmail.com>
2019-08-19 15:48:40 +0300

    import race

--- salalem_notifications/apps.py
+++ salalem_notifications/apps.py
@@ -5,12 +5,19 @@ import sys
 from django.apps import AppConfig
 from django.conf import settings
 
+
 class Config(AppConfig):
     name = "salalem_notifications"
 
     def ready(self):
         super(Config, self).ready()
-        if settings.IS_NOTIFICATION_SERVICE:
+        IS_NOTIFICATION_SERVICE = None
+        try:
+            IS_NOTIFICATION_SERVICE = settings.IS_NOTIFICATION_SERVICE
+        except:
+            pass
+
+        if IS_NOTIFICATION_SERVICE:
             if os.environ.get("BUILD_PROFILE", "") != "development":
                 import lms_events_handlers.handlers
 

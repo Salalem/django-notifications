9be84adef8b3c35bcda653b12a4e68d038029677
fkafri <firas.alkafri@gmail.com>
2019-08-28 14:24:17 +0300

    django static root

--- salalem_notifications/urls.py
+++ salalem_notifications/urls.py
@@ -1,11 +1,13 @@
 import os
 
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
 
 if os.environ.get("BUILD_PROFILE", "") == "development":
     import lms_events_handlers.handlers
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
